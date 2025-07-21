fde92448b61df3c1c36861aeff044a6644fb825c
princebatubo <pdawaribatubo@gmail.com>
2025-07-21 15:37:21 +0100

    Switch to @dodopayments/node

--- src/lib/dodopayments.ts
+++ src/lib/dodopayments.ts
@@ -1,9 +1,10 @@
-import DodoPayments from "dodopayments";
+import { DodoPayments } from "@dodopayments/node";
+
 export const dodopayments = new DodoPayments({
-  bearerToken:
+  apiKey:
     process.env.NODE_ENV === "development"
       ? process.env.DODO_API_KEY_TEST
-      : process.env.DODO_API_KEY_LIVE, // This is the default and can be omitted if env is named as DODO_PAYMENTS_API_KEY
+      : process.env.DODO_API_KEY_LIVE,
   environment:
-    process.env.NODE_ENV === "development" ? "test_mode" : "live_mode", // defaults to 'live_mode'
+    process.env.NODE_ENV === "development" ? "test_mode" : "live_mode",
 });
